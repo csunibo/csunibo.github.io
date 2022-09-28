01c2a268ae8db79fb183c65fd29cdb6d7f3e765d
kocierik <kocierik@gmail.com>
2022-09-28 18:57:15 +0200

    feat: add PWA

---
+++ sw.js
@@ -0,0 +1,48 @@
+var APP_PREFIX = 'dicethrow_';
+var VERSION = 'version_06';
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+  '/csunibo.github.io/',
+  '/csunibo.github.io/index.html'
+]
+
+self.addEventListener('fetch', function(e) {
+  console.log('fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url);
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      var cacheWhitelist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function(key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i]);
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
