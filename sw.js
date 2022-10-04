b1728c057450bcdb1dd78ced63c8023066e98689
kocierik <kocierik@gmail.com>
2022-10-04 12:18:56 +0200

    feat: add course page

--- sw.js
+++ sw.js
@@ -4,7 +4,29 @@ var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
   '/csunibo.github.io/',
   '/csunibo.github.io/index.html',
-  '/csunibo.github.io/programmazione/'
+  '/csunibo.github.io/programmazione/',
+  '/csunibo.github.io/architettura-degli-elaborati/',
+  '/csunibo.github.io/logica-per-informatica/',
+  '/csunibo.github.io/algebra-e-geometria/',
+  '/csunibo.github.io/algoritmi-e-strutture-di-dati/',
+  '/csunibo.github.io/analisi-matematica/',
+  '/csunibo.github.io/calcolo-numerico/',
+  '/csunibo.github.io/ottimizzazione-combinatoria/',
+  '/csunibo.github.io/calcolo-delle-probabilita-e-statistica/',
+  '/csunibo.github.io/tecnologie-web/',
+  '/csunibo.github.io/linguaggi-di-programmazione/',
+  '/csunibo.github.io/reti-di-calcolatori/',
+  '/csunibo.github.io/sistemi-operativi/',
+  '/csunibo.github.io/basi-di-dati/',
+  '/csunibo.github.io/informatica-teorica/',
+  '/csunibo.github.io/ingegneria-del-software/',
+  '/csunibo.github.io/introduzione-apprendimento-automatico/',
+  '/csunibo.github.io/fondamenti-di-cibersecurity/',
+  '/csunibo.github.io/progetto-di-sistemi-virtuali/',
+  '/csunibo.github.io/fisica/',
+  '/csunibo.github.io/laboratorio-di-applicazioni-mobili/',
+  '/csunibo.github.io/storia-informatica-e-dei-dispositivi-di-calcolo/',
+  '/csunibo.github.io/strategie-aziendali/',
 ]
 
 self.addEventListener('fetch', function(e) {
